50bc40120ee4b60f0537cffb67b6aca89f81dc65
Geoff Pascoe <pascoeg@gmail.com>
2013-10-11 14:15:21 +0100

    decoupled login state from user being viewed

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.di
 class PubsHandler(webapp2.RequestHandler):
   def show_all_pubs(self):
     pubs = Pub.all().run()
-    values = {'pubs' : pubs, 'user' : None}
+    values = {'pubs' : pubs, 'logged_in' : False}
     template = jinja_environment.get_template('templates/pubs.html')
     self.response.out.write(template.render(values))
 
@@ -23,40 +23,43 @@ class PubsHandler(webapp2.RequestHandler):
       return self.show_all_pubs()
     visited = Visit.all().ancestor(user).filter('visited =', True).fetch(10)
     not_visited = Visit.all().ancestor(user).filter('visited =',False).fetch(10)
-    values = {'visited' : visited, 'not_visited' : not_visited, 'user' : user}
+    values = {'visited' : visited, 'not_visited' : not_visited, 'logged_in' : True}
     template = jinja_environment.get_template('templates/user_pubs.html')
     self.response.out.write(template.render(values)) 
 
 class UserHandler(webapp2.RequestHandler):
   def get(self, user_id=None):
+    current_user = get_current_user()
     if user_id:
       user = User.get_by_id(int(user_id))
     else:
-      user = get_current_user()
+      user = current_user
     visits = db.GqlQuery('SELECT __key__ FROM Visit WHERE ANCESTOR IS :1 AND visited = :2', user, True).count()
-    values = {'visits' : visits, 'user' : user}
+    values = {'visits' : visits, 'user' : user, 'logged_in' : current_user is not None}
     template = jinja_environment.get_template('templates/user.html')
     self.response.out.write(template.render(values))
 
 class VisitedHandler(webapp2.RequestHandler):
   def get(self, user_id=None):
+    current_user = get_current_user()
     if user_id:
       user = User.get_by_id(int(user_id))
     else:
-      user = get_current_user()
+      user = current_user
     visits = Visit.all().ancestor(user).filter('visited =', True).run()
-    values = {'visited' : visits, 'user' : user}
+    values = {'visited' : visits, 'user' : user, 'logged_in' : current_user is not None}
     template = jinja_environment.get_template('templates/user_pubs.html')
     self.response.out.write(template.render(values))
 
 class NotVisitedHandler(webapp2.RequestHandler):
   def get(self, user_id=None):
+    current_user = get_current_user()
     if user_id:
       user = User.get_by_id(int(user_id))
     else:
-      user = get_current_user()
+      user = current_user
     visits = Visit.all().ancestor(user).filter('visited =', False).run()
-    values = {'not_visited' : visits, 'user' : user}
+    values = {'not_visited' : visits, 'user' : user, 'logged_in' : current_user is not None}
     template = jinja_environment.get_template('templates/user_pubs.html')
     self.response.out.write(template.render(values))
     
@@ -70,6 +73,7 @@ class PubHandler(webapp2.RequestHandler):
     if user:
       visit = Visit.all().ancestor(user).filter('pub =', pub).get()
       values['visited'] = visit.visited
+      values['logged_in'] = True
 
     template = jinja_environment.get_template('templates/pub.html')
     self.response.out.write(template.render(values))
