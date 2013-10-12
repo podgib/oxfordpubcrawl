f39f252a1471b0164e6da6c1ac1c9e195af6940c
Geoff Pascoe <pascoeg@gmail.com>
2013-10-13 00:45:59 +0100

    sort pubs by name when creating list for nearby

--- src/ajax.py
+++ src/ajax.py
@@ -31,6 +31,7 @@ class ClosestHandler(webapp2.RequestHandler):
       visits = memcache.get('not-visited-' + str(user.key().id()))
       if not visits:
         visits = Visit.all().ancestor(user).filter('visited =', False).fetch(500)
+        visits = sorted(visits, key = lambda v: v.pub.name.lower())
         memcache.set('not-visited-' + str(user.key().id()), visits)
       visits = sorted(visits, key=lambda v: v.pub.distance(lat, long))[0:5]
       pubs = [v.pub.toDictionary() for v in visits]
@@ -38,6 +39,7 @@ class ClosestHandler(webapp2.RequestHandler):
       pubs = memcache.get('all-pubs-list')
       if not pubs:
         pubs = Pub.all().fetch(500)
+        pubs = sorted(pubs, key = lambda p: p.name.lower())
         memcache.set('all-pubs-list', pubs)
       pubs = sorted(pubs, key=lambda p: p.distance(lat, long))[0:5]
       pubs = [p.toDictionary() for p in pubs]
